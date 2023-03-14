ea460b38f07b0c83ffb03aada67de700f20ec8fe
Greg Roques <greg.roques@gmail.com>
2023-03-13 20:41:45 -0400

    update

--- clearmem/clearmem.asm
+++ clearmem/clearmem.asm
@@ -13,5 +13,34 @@
 .byte $00				; No PRG-RAM
 .byte $00,$00,$00,$00,$00		; unused padding to complete the 16 bytes of header
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PRG-ROM, always located at $8000
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 .segment "CODE"
-	
+.org $8000				; start of PRG-ROM
+
+RESET:
+  sei					; disables all interupt requests
+  cld					; clears the decimal mode... even though we don't have it, it is customary to do this
+  ldx #$FF
+  txs					; Initialize the stack pointer at $01FF, at the bottom of our stack
+
+  ldx #0				; A = 0
+  ldx #$FF				; X = $FF
+MemLoop:
+  sta $0,x				; store the value of A (Zero) into $0 + x
+  dex					; x--
+  be MemLoop				; if X is not zero, we loop back to the MemLoop label
+
+NMI:
+  rti					; return
+
+IRQ:
+  rti
+
+.segment "VECTORS" 			; Vectors point to handlers in code
+.org $FFFA 
+.word NMI 				; .word defines address, or label
+.word RESET
+.word IRQ	
